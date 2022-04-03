00e30fa256271ba4e6dfc774f4d8df1aaaf85630
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-03 20:31:03 +0200

    dod. dzialajaca konwersje dla typu angle (kat), testowac, poprawiac

--- my-app/src/convertionTypes/angle/AngleConverter.tsx
+++ my-app/src/convertionTypes/angle/AngleConverter.tsx
@@ -1,44 +1,69 @@
-import React, { useState } from "react";
-import Angle from "./convertingFns/Angle";
-// import myRound from "../../utils/myRound";
+import React, { ReactElement, useEffect, useState } from "react";
+import { turns2units, units2turns } from "./convertingFns/converter";
 
-// function getOption(value: string) {
-//     return <option value={value}>{value}</option>;
-// }
+interface unitsChoice {
+    id: number;
+    unit: string;
+}
 
 const AngleConverter: React.FC = () => {
     const [input, setInput] = useState("0");
-    const [output, setOutput] = useState("0");
-    const angle: Angle = new Angle(0);
+    const [inUnits, setInUnits] = useState("degrees");
+    const [angleTurns, setAngleTurns] = useState(0);
 
-    // const units: string[] = [
-    //     "degrees", "gradians", "mils", "radians"
-    // ];
+    const units: unitsChoice[] = [
+        { id: 0, unit: "degrees" },
+        { id: 1, unit: "gradians" },
+        { id: 2, unit: "mils" },
+        { id: 3, unit: "radians" },
+    ];
+
+    const getOption = (unit: unitsChoice): ReactElement<HTMLElement> => {
+        return (
+            <option key={unit.id} value={unit.unit}>
+                {unit.unit}
+            </option>
+        );
+    };
 
     const handleTyping = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setInput(event.target.value);
-        angle.degrees = parseFloat(event.target.value);
-        setOutput(angle.mils.toFixed(2));
+    };
+
+    const handleOption = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+        setInUnits(event.target.value);
+    };
+
+    useEffect(() => {
+        const handleConversion = (): void => {
+            setAngleTurns(units2turns(parseFloat(input), inUnits));
+        };
+        handleConversion();
+    }, [angleTurns, input, inUnits]);
+
+    const getOutput = (unit: unitsChoice): ReactElement<HTMLElement> => {
+        return (
+            <p key={unit.id}>
+                {" "}
+                {turns2units(angleTurns, unit.unit).toFixed(2)} {unit.unit}
+            </p>
+        );
     };
 
     return (
-        <div >
-            <input name="" type="text" value={input} onChange={handleTyping} /> degrees
-            {/*
-            <form >
-                <label>Choose unit</label>
+        <div>
+            <input name="" type="text" value={input} onChange={handleTyping} />
+            <br /> <br />
+            <form>
                 &nbsp; &nbsp;
-                <select value={units}>
-                    {units.map(unit => getOption(unit))}
+                <select value={inUnits} onChange={handleOption}>
+                    {units.map((unit) => getOption(unit))}
                 </select>
-                <br /> <br />
-                <input type="submit" value="Go To the Game" />
             </form>
-	     */}
             <h1>&#187;</h1>
-            <p>{output} mils</p>
+            <div>{units.map((u) => getOutput(u))}</div>
         </div>
-    )
-}
+    );
+};
 
 export default AngleConverter;
