96669d003a6a88179d0e479ae6600a6dfc491312
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-15 10:03:06 +0200

    kosmetyka

--- my-app/src/convertionTypes/temperature/convertingFns/celsius.ts
+++ my-app/src/convertionTypes/temperature/convertingFns/celsius.ts
@@ -1,20 +1,18 @@
+import isOutOfRange from "./isOutOfRange";
 import { KELVINMIN, KELVINMAX, k2c } from "./kelvin";
+
 const CELSIUSMIN: number = k2c(KELVINMIN); // absolute zero
 // max is: Plank-Temperature = Math.pow(10, 32) Kelvin
 // here I will give somewhat less
 const CELSIUSMAX: number = k2c(KELVINMAX);
 
-function isOutOfRange(celsius: number): boolean {
-    if (celsius < CELSIUSMIN || celsius > CELSIUSMAX) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 function c2k(celsius: number): number {
-    // kelvin
-    return isOutOfRange(celsius) ? NaN : (celsius + 273.15);
+    // to kelvin
+    if (isOutOfRange(celsius, CELSIUSMIN, CELSIUSMAX)) {
+        return NaN;
+    } else {
+        return (celsius + 273.15);
+    }
 }
 
 export { c2k };
