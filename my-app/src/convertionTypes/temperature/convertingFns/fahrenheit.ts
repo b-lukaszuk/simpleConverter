62e4767dff2a36a9ade381b1634b998e4493257f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-15 10:45:26 +0200

    kosmetyka

--- my-app/src/convertionTypes/temperature/convertingFns/fahrenheit.ts
+++ my-app/src/convertionTypes/temperature/convertingFns/fahrenheit.ts
@@ -1,20 +1,17 @@
+import isOutOfRange from "./isOutOfRange";
 import { KELVINMIN, KELVINMAX, k2f } from "./kelvin";
 const FAHRENHEITMIN: number = k2f(KELVINMIN); // absolute zero
 // max is: Plank-Temperature = Math.pow(10, 32) Kelvin
 // here I will give somewhat less
 const FAHRENHEITMAX: number = k2f(KELVINMAX);
 
-function isOutOfRange(fahrenheit: number): boolean {
-    if (fahrenheit < FAHRENHEITMIN || fahrenheit > FAHRENHEITMAX) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 function f2k(fahrenheit: number): number {
-    // kelvin
-    return isOutOfRange(fahrenheit) ? NaN : ((fahrenheit + 459.67) * (5 / 9));
+    // to kelvin
+    if (isOutOfRange(fahrenheit, FAHRENHEITMIN, FAHRENHEITMAX)) {
+        return NaN;
+    } else {
+        return ((fahrenheit + 459.67) * (5 / 9));
+    }
 }
 
 export { f2k };
