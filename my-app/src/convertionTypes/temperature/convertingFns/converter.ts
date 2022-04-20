46f01195bcd00e00e1fde1e8dfecde75d7b0eb23
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-20 19:07:13 +0200

    poprawienie bledow (literowek w konwerterach temperatur)

--- my-app/src/convertionTypes/temperature/convertingFns/converter.ts
+++ my-app/src/convertionTypes/temperature/convertingFns/converter.ts
@@ -15,19 +15,19 @@ interface unitsChoice {
 function kelvin2units(howMany: number, units: string): number {
     switch (units) {
         case "celsius":
-            return c2k(howMany);
+            return k2c(howMany);
         case "delisle":
-            return d2k(howMany);
+            return k2d(howMany);
         case "fahrenheit":
-            return f2k(howMany);
+            return k2f(howMany);
         case "newton":
-            return n2k(howMany);
+            return k2n(howMany);
         case "rankine":
-            return ra2k(howMany);
-        case "reamur":
-            return re2k(howMany);
+            return k2ra(howMany);
+        case "reaumur":
+            return k2re(howMany);
         case "romer":
-            return ro2k(howMany);
+            return k2ro(howMany);
         case "kelvin":
             return howMany;
         default:
@@ -38,19 +38,19 @@ function kelvin2units(howMany: number, units: string): number {
 function units2kelvin(howMany: number, units: string): number {
     switch (units) {
         case "celsius":
-            return k2c(howMany);
+            return c2k(howMany);
         case "delisle":
-            return k2d(howMany);
+            return d2k(howMany);
         case "fahrenheit":
-            return k2f(howMany);
+            return f2k(howMany);
         case "newton":
-            return k2n(howMany);
+            return n2k(howMany);
         case "rankine":
-            return k2ra(howMany);
-        case "reamur":
-            return k2re(howMany);
+            return ra2k(howMany);
+        case "reaumur":
+            return re2k(howMany);
         case "romer":
-            return k2ro(howMany);
+            return ro2k(howMany);
         case "kelvin":
             return howMany;
         default:
