a63bd569706b06066192462f01156d36660da296
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-30 09:24:31 +0200

    kosmetyka

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -31,7 +31,7 @@ const App: React.FC = (): ReactElement<HTMLElement> => {
         <div className="App">
             <h1>Simple Converter</h1>
             <p className="warning">NO GUARANTEE OF CORRECT CONVERSION. USE IT AT YOUR OWN RISK</p>
-            <p>(Still, I hope would work fine. Some rounding errors are possible)</p>
+            <p>(Still, I hope it will work fine. Some rounding errors are possible)</p>
             <hr />
             <p>Select conversion type:</p>
             <select value={convChoice} onChange={handleConvertionChange}>
