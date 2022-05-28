95622506ea0501c63fbfda0e6037ae191ea8a436
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-28 21:13:14 +0200

    poprawka blednej konwersji f3 -> m3

--- my-app/src/convertionTypes/volume/cubicFoot.ts
+++ my-app/src/convertionTypes/volume/cubicFoot.ts
@@ -11,7 +11,7 @@ const CUBFOOTMAX: number = cubicMetreToCubicFoot(CUBMETREMAX);
 function cubicFootToCubicMetre(foot3: number): number {
     return convertIfInRange(
         foot3,
-        (f3: number): number => f3 / 1e6,
+        (f3: number): number => f3 * 0.028316846592,
         CUBFOOTMIN,
         CUBFOOTMAX
     );
