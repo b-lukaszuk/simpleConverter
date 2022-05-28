5cbec09280e7c9a133e5840a0bfc060539fb9601
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-28 19:18:26 +0200

    dodano fnsy konwertujace z m3 do secondary units

--- my-app/src/convertionTypes/volume/cubicMetre.ts
+++ my-app/src/convertionTypes/volume/cubicMetre.ts
@@ -1,1 +1,82 @@
-// todo
+import { convertIfInRange } from "../../utils/convertIfInRange";
+
+const CUBMETREMIN: number = 0;
+const CUBMETREMAX: number = 1e15;
+
+// mostly based on en.wikipedia + some google searches
+
+// fluid barrel
+function cubicMetreToBarrel(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 / 0.158987294928,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToCubicCentimetre(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 * 1e6,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToCubicFoot(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 / 0.028316846592,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToInch(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 / 0.000016387064,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToUsGallon(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 / 0.003785411784,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToLitre(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 * 1e3,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+function cubicMetreToPint(metre3: number): number {
+    return convertIfInRange(
+        metre3,
+        (m3: number): number => m3 / 0.000473176473,
+        CUBMETREMIN,
+        CUBMETREMAX
+    );
+}
+
+export {
+    CUBMETREMIN,
+    CUBMETREMAX,
+    cubicMetreToBarrel,
+    cubicMetreToCubicCentimetre,
+    cubicMetreToCubicFoot,
+    cubicMetreToInch,
+    cubicMetreToUsGallon,
+    cubicMetreToLitre,
+    cubicMetreToPint,
+};
